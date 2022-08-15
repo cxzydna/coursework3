4a270a4a8226dcf704c0f4e645a67094c51e2278
Gleb Kushedow <kushedow@gmail.com>
2022-08-15 12:50:45 +0300

    rename data to posts

---
+++ app.py
@@ -0,0 +1,43 @@
+import utils
+
+from flask import Flask, render_template, request
+from post_blueprint.post_bp import post_bp
+
+app = Flask(__name__)
+#app.register_blueprint(post_bp, url_prefix='/post')
+
+
+@app.route('/', methods=['GET'])
+def main_page():
+    posts = utils.get_posts_all()
+    return render_template('index.html', posts=posts)
+
+
+@app.route('/post/<int:post_id>/', methods=['GET'])
+def post_page(post_id):
+    comments = utils.get_comments_by_post_id(post_id)
+    post = utils.get_post_by_comment(post_id)
+    counter = len(comments)
+    return render_template(
+        'post.html', comments=comments, post=post, comments_count=counter
+    )
+
+
+@app.route('/search/', methods=['GET'])
+def search_post():
+    query = request.args.get("s")
+    posts = utils.search_for_posts(query)
+    posts_count = len(posts)
+    return render_template(
+        'search.html', posts=posts, posts_count=posts_count, query=query
+    )
+
+
+@app.route('/users/<username>/', methods=['GET'])
+def user_page(username):
+    posts = utils.get_posts_by_user(username)
+    return render_template('user-feed.html', posts=posts, username=username)
+
+
+if __name__ == '__main__':
+    app.run(debug=True, port=5000)
---
+++ utils.py
@@ -0,0 +1,61 @@
+import json
+
+
+def get_posts_all():
+    with open("data/posts.json", "r", encoding="utf-8") as f:
+        posts = json.load(f)
+    return posts
+
+
+def get_all_comments():
+    with open("data/comments.json", "r", encoding="utf-8") as f:
+        comments = json.load(f)
+    return comments
+
+
+def get_posts_by_user(user_name):
+    all_posts = get_posts_all()
+    result = []
+    try:
+        for post in all_posts:
+            if post["poster_name"].lower() == user_name.lower():
+                result.append(post)
+    except ValueError:
+        print('Такого пользователя нет')
+    return result
+
+
+def get_comments_by_post_id(post_id):
+    all_comments = get_all_comments()
+    result = []
+    try:
+        for comment in all_comments:
+            if comment["post_id"] == post_id:
+                result.append(comment)
+    except ValueError:
+        print('Такого поста нет')
+    return result
+
+
+def search_for_posts(query):
+    all_posts = get_posts_all()
+    result = []
+
+    for post in all_posts:
+        if query in post["content"]:
+            result.append(post)
+    return result if len(result) <= 10 else []
+
+
+def get_post_by_pk(pk):
+    all_posts = get_posts_all()
+    for post in all_posts:
+        if post["pk"] == pk:
+            return post
+
+
+def get_post_by_comment(post_id):
+    all_posts = get_posts_all()
+    for post in all_posts:
+        if post_id == post["pk"]:
+            return post
