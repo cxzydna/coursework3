fa34e6aa0068080e4ab3a69711b9b5312378243e
Andy <blackwhite901@mail.ru>
2023-01-11 09:44:09 +0300

    rename data to posts

--- app.py
+++ app.py
@@ -4,7 +4,6 @@ from flask import Flask, render_template, request
 from post_blueprint.post_bp import post_bp
 
 app = Flask(__name__)
-#app.register_blueprint(post_bp, url_prefix='/post')
 
 
 @app.route('/', methods=['GET'])
